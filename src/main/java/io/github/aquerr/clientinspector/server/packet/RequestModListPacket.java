e2444a63fc9dc7acb28561233d09d7e4d67cd268
Aquerr <nerdianin@gmail.com>
2023-06-21 18:31:29 +0200

    Fix crash that occurred while detecting mods. Config cleanup.

--- src/main/java/io/github/aquerr/clientinspector/server/packet/RequestModListPacket.java
+++ src/main/java/io/github/aquerr/clientinspector/server/packet/RequestModListPacket.java
@@ -4,7 +4,8 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraftforge.fml.loading.moddiscovery.BackgroundScanHandler;
 import net.minecraftforge.fml.loading.moddiscovery.ClasspathLocator;
 import net.minecraftforge.fml.loading.moddiscovery.ModDiscoverer;
-import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
+import net.minecraftforge.fml.loading.moddiscovery.ModFile;
+import net.minecraftforge.fml.loading.moddiscovery.ModFileInfo;
 import net.minecraftforge.fml.loading.moddiscovery.ModsFolderLocator;
 import net.minecraftforge.forgespi.language.IModFileInfo;
 import net.minecraftforge.forgespi.language.IModInfo;
@@ -17,10 +18,11 @@ import org.apache.logging.log4j.Logger;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 public class RequestModListPacket implements ClientInspectorPacket
 {
@@ -39,42 +41,56 @@ public class RequestModListPacket implements ClientInspectorPacket
     public static void handlePacket(RequestModListPacket modListPacket, Supplier<NetworkEvent.Context> contextSupplier)
     {
         LOGGER.info("Sending mod-list packet to the server...");
-        ModDiscoverer modDiscoverer = new ModDiscoverer(Collections.emptyMap());
-
-//        Launcher.INSTANCE.
-
-//         Thanks to forge team for making this utility static method :D
-//        final List<String> modList = modDiscoverer.discoverMods().getLoadingModList()
-//                        .getMods()
-//                        .stream()
-//                        .map(ModInfo::getModId)
-//                        .collect(Collectors.toList());
-
+        ModDiscoverer modDiscoverer = new ModDiscoverer(prepareArguments());
 
         ModsFolderLocator modsFolderLocator = new ModsFolderLocator();
         modsFolderLocator.initArguments(Collections.emptyMap());
         ClasspathLocator classpathLocator = new ClasspathLocator();
         classpathLocator.initArguments(Collections.emptyMap());
 
-        LinkedList<IModLocator.ModFileOrException> modFiles = new LinkedList<>();
-        modFiles.addAll(modsFolderLocator.scanMods());
-        modFiles.addAll(classpathLocator.scanMods());
-
-        BackgroundScanHandler scanHandler = new BackgroundScanHandler(modDiscoverer.discoverMods().stage2Validation().getModFiles());
-
-        List<ModInfo> modInfos = scanHandler.getLoadingModList().getMods();
-        LOGGER.info(modInfos);
-
-        List<String> modNames = modFiles.stream()
-                .filter(modFileOrException -> modFileOrException.ex() == null)
+        LinkedList<IModFileInfo> modFiles = new LinkedList<>();
+        modFiles.addAll(modsFolderLocator.scanMods().stream().map(IModLocator.ModFileOrException::file)
+                .map(IModFile::getModFileInfo)
+                .toList());
+        modFiles.addAll(classpathLocator.scanMods().stream()
                 .map(IModLocator.ModFileOrException::file)
                 .map(IModFile::getModFileInfo)
+                .toList());
+
+        BackgroundScanHandler scanHandler = modDiscoverer.discoverMods().stage2Validation();
+        modFiles.addAll(scanHandler.getLoadingModList().getModFiles().stream()
+                .map(ModFileInfo::getFile)
+                .map(ModFile::getModFileInfo)
+                .toList()
+        );
+
+        if (LOGGER.isDebugEnabled())
+        {
+            LOGGER.debug("Found mod files: {}", modFiles.stream()
+                    .map(IModFileInfo::getFile)
+                    .map(IModFile::getFilePath)
+                    .toList());
+        }
+
+        List<String> modIds = modFiles.stream()
                 .map(IModFileInfo::getMods)
                 .flatMap(Collection::stream)
                 .map(IModInfo::getModId)
-                .collect(Collectors.toList());
+                .toList();
 
-        LOGGER.info("Found mod-list: " + Arrays.toString(modNames.toArray()));
-        ClientInspectorPacketRegistry.INSTANCE.sendToServer(new ModListPacket(modNames));
+        if (LOGGER.isDebugEnabled())
+        {
+            LOGGER.debug("Found mod-list: {}", Arrays.toString(modIds.toArray()));
+        }
+        ClientInspectorPacketRegistry.INSTANCE.sendToServer(new ModListPacket(modIds));
+    }
+
+    private static Map<String, ?> prepareArguments()
+    {
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("mods", Collections.emptyList());
+        arguments.put("modLists", Collections.emptyList());
+        arguments.put("mavenRoots", Collections.emptyList());
+        return arguments;
     }
 }
