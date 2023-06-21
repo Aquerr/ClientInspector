e581fc16ffdfb04db60d249a183c94c9b232b96d
Aquerr <nerdianin@gmail.com>
2023-06-21 22:28:27 +0200

    Fixes and cleanup for mod scanning.

--- src/main/java/io/github/aquerr/clientinspector/server/packet/RequestModListPacket.java
+++ src/main/java/io/github/aquerr/clientinspector/server/packet/RequestModListPacket.java
@@ -1,12 +1,9 @@
 package io.github.aquerr.clientinspector.server.packet;
 
-import cpw.mods.modlauncher.Launcher;
 import net.minecraft.network.PacketBuffer;
-import net.minecraftforge.fml.loading.FMLLoader;
-import net.minecraftforge.fml.loading.FMLServiceProvider;
 import net.minecraftforge.fml.loading.moddiscovery.BackgroundScanHandler;
 import net.minecraftforge.fml.loading.moddiscovery.ModDiscoverer;
-import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
+import net.minecraftforge.fml.loading.moddiscovery.ModFileInfo;
 import net.minecraftforge.fml.loading.moddiscovery.ModsFolderLocator;
 import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.forgespi.language.IModFileInfo;
@@ -16,7 +13,13 @@ import net.minecraftforge.userdev.ClasspathLocator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -37,17 +40,7 @@ public class RequestModListPacket implements ClientInspectorPacket
     public static void handlePacket(RequestModListPacket modListPacket, Supplier<NetworkEvent.Context> contextSupplier)
     {
         LOGGER.info("Sending mod-list packet to the server...");
-        ModDiscoverer modDiscoverer = new ModDiscoverer(Collections.emptyMap());
-
-        Launcher.INSTANCE.
-
-        // Thanks to forge team for making this utility static method :D
-//        final List<String> modList = modDiscoverer.discoverMods().getLoadingModList()
-//                        .getMods()
-//                        .stream()
-//                        .map(ModInfo::getModId)
-//                        .collect(Collectors.toList());
-
+        ModDiscoverer modDiscoverer = new ModDiscoverer(prepareArguments());
 
         ModsFolderLocator modsFolderLocator = new ModsFolderLocator();
         modsFolderLocator.initArguments(Collections.emptyMap());
@@ -58,18 +51,37 @@ public class RequestModListPacket implements ClientInspectorPacket
         modFiles.addAll(modsFolderLocator.scanMods());
         modFiles.addAll(classpathLocator.scanMods());
 
-        BackgroundScanHandler scanHandler = new BackgroundScanHandler(modDiscoverer.discoverMods().getModFiles());
+        BackgroundScanHandler scanHandler = modDiscoverer.discoverMods();
+        modFiles.addAll(scanHandler.getLoadingModList().getModFiles().stream()
+                .map(ModFileInfo::getFile)
+                .collect(Collectors.toList()));
 
-        List<ModInfo> modInfos = scanHandler.getLoadingModList().getMods();
+        if (LOGGER.isDebugEnabled())
+        {
+            LOGGER.debug("Found mod files: {}", modFiles.stream()
+                    .map(IModFile::getFilePath)
+                    .collect(Collectors.toList()));
+        }
 
-        List<String> modNames = modFiles.stream()
+        List<String> modIds = modFiles.stream()
                 .map(IModFile::getModFileInfo)
                 .map(IModFileInfo::getMods)
                 .flatMap(Collection::stream)
                 .map(IModInfo::getModId)
                 .collect(Collectors.toList());
 
-        LOGGER.info("Found mod-list: " + Arrays.toString(modNames.toArray()));
-        ClientInspectorPacketRegistry.INSTANCE.sendToServer(new ModListPacket(modNames));
+        if (LOGGER.isDebugEnabled())
+        {
+            LOGGER.debug("Found mod-list: {}", Arrays.toString(modIds.toArray()));
+        }
+    }
+
+    private static Map<String, ?> prepareArguments()
+    {
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("mods", Collections.emptyList());
+        arguments.put("modLists", Collections.emptyList());
+        arguments.put("mavenRoots", Collections.emptyList());
+        return arguments;
     }
 }
