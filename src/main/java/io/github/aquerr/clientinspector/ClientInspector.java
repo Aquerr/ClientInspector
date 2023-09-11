654b567a93e065cf445c31c47f4a655abd66e35a
Aquerr <nerdianin@gmail.com>
2023-09-11 19:23:00 +0200

    Generate config file only on server side.

--- src/main/java/io/github/aquerr/clientinspector/ClientInspector.java
+++ src/main/java/io/github/aquerr/clientinspector/ClientInspector.java
@@ -3,8 +3,8 @@ package io.github.aquerr.clientinspector;
 import io.github.aquerr.clientinspector.server.ServerProxy;
 import io.github.aquerr.clientinspector.server.packet.ClientInspectorPacketRegistry;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,17 +20,19 @@ public class ClientInspector
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::serverSetup);
     }
 
     private void setup(final FMLCommonSetupEvent event)
     {
         LOGGER.info("Initializing " + ID);
-
-//        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Configuration);
-
-        ServerProxy.init();
         ClientInspectorPacketRegistry.registerPackets();
+    }
 
+    private void serverSetup(final FMLDedicatedServerSetupEvent event)
+    {
+        LOGGER.info("Initializing server side {}", ID);
+        ServerProxy.init();
         LOGGER.info("Mod load completed!");
     }
 }
