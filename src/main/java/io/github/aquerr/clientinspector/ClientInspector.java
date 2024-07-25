c24a126ecd2313746d91bab01cd5e93b14f4ca69
Aquerr <nerdianin@gmail.com>
2024-07-25 18:30:05 +0200

    Ability to populate mods_to_detect with server mods on startup (new config populate_mods_to_detect_with_server_mods)

--- src/main/java/io/github/aquerr/clientinspector/ClientInspector.java
+++ src/main/java/io/github/aquerr/clientinspector/ClientInspector.java
@@ -5,6 +5,7 @@ import io.github.aquerr.clientinspector.server.packet.ClientInspectorPacketRegis
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,11 +17,14 @@ public class ClientInspector
 
     private static final Logger LOGGER = LogManager.getLogger();
 
+    private boolean isServer;
+
     public ClientInspector()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::serverSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::loadComplete);
     }
 
     private void setup(final FMLCommonSetupEvent event)
@@ -31,8 +35,16 @@ public class ClientInspector
 
     private void serverSetup(final FMLDedicatedServerSetupEvent event)
     {
+        isServer = true;
         LOGGER.info("Initializing server side {}", ID);
         ServerProxy.init();
         LOGGER.info("Mod load completed!");
+    }
+
+    private void loadComplete(final FMLLoadCompleteEvent event)
+    {
+        if (isServer) {
+            ServerProxy.loadCompleted();
+        }
     }
 }
